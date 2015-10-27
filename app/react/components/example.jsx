fc00f5934e539ca40f22af150c4b4398c8242d84
Mateusz Palyz <mateusz.palyz@netguru.pl>
2015-10-27 14:56:52 +0100

    init commit

---
+++ app/react/components/example.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import moment from 'moment';
+import DateRangePicker from 'react-bootstrap-daterangepicker';
+
+class Example extends React.Component {
+  render() {
+    return (
+      <DateRangePicker>
+        <div>Click me, I'm a daterangepicker!</div>
+      </DateRangePicker>
+    );
+  }
+}
+
+export default Example;
---
+++ app/react/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+window.React = React;
+window.ReactDOM = ReactDOM;
+
+import Example from './components/example';
+registerComponent('Example', Example);
